f5a2f429e53bb66c48583c716951e6819be39e81
Yuliana Solari <yuliana.solari.oporto@gmail.com>
2026-03-19 12:23:17 -0500

    Primer Avance

--- app/src/main/java/com/example/appmascotas/ListarCustom.java
+++ app/src/main/java/com/example/appmascotas/ListarCustom.java
@@ -1,24 +1,102 @@
 package com.example.appmascotas;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class ListarCustom extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class ListarCustom extends AppCompatActivity implements MascotaAdapter.OnAccionListener {
+
+    RecyclerView recyclerMascota;
+    MascotaAdapter adapter;
+    ArrayList<Mascota> listaMascotas;
+
+    RequestQueue requestQueue;
+    private final String URL ="http://192.168.56.1:3000/mascotas";
+
+    private void loadUI(){
+        recyclerMascota= findViewById(R.id.recyclerMascotas);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_listar_custom);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        loadUI();
+
+        listaMascotas = new ArrayList<>();
+        adapter= new MascotaAdapter(this, listaMascotas, this);
+
+        recyclerMascota.setLayoutManager(new LinearLayoutManager(this));
+        recyclerMascota.setAdapter(adapter);
+
+        obtenerDatos();
+    }
+
+    private void obtenerDatos(){
+        requestQueue = Volley.newRequestQueue(this);
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                URL,
+                null,
+                jsonArray -> renderizarLista(jsonArray),
+                error -> {
+                    Log.e("ErrorWs", error.toString());
+                    Toast.makeText(this, "No se obtuvieron los datos", Toast.LENGTH_SHORT).show();
+                }
+        );
+
+        requestQueue.add(jsonArrayRequest);
+    }
+
+    private void renderizarLista(JSONArray jsonmascota){
+        try {
+            listaMascotas.clear();
+
+            for (int i = 0; i < jsonmascota.length(); i++){
+                JSONObject json = jsonmascota.getJSONObject(i);
+
+                listaMascotas.add(new Mascota(
+                        json.getInt("id"),
+                        json.getString("tipo"),
+                        json.getString("nombre"),
+                        json.getString("color"),
+                        json.getDouble("pesokg")
+                ));
+            }
+
+            adapter.notifyDataSetChanged();
+
+        } catch (Exception e){
+           Log.e("ErrorJSON", e.toString());
+        }
+    }
+
+    @Override
+    public void onEditar(int posicion, Mascota mascota) {
+        Toast.makeText(this, "Editar: " + mascota.getId(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onEliminar(int position, Mascota mascota) {
+        adapter.eliminarItem(position);
+        Toast.makeText(this, "Eliminado: " + mascota.getNombre(), Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/appmascotas/MascotaAdapter.java
+++ app/src/main/java/com/example/appmascotas/MascotaAdapter.java
@@ -10,45 +10,28 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 
-import javax.crypto.Mac;
-
-//RecyclerViewAdapter exige 3 metodos
 public class MascotaAdapter extends RecyclerView.Adapter<MascotaAdapter.ViewHolder> {
 
     private final ArrayList<Mascota> lista;
     private final Context context;
-
     private final OnAccionListener listener;
 
-    //Interface
-
     public interface OnAccionListener{
-       void onEditar (int posicion, Mascota mascota);
-       void onEliminar (int position, Mascota mascota);
-
+        void onEditar (int posicion, Mascota mascota);
+        void onEliminar (int position, Mascota mascota);
     }
 
-
-  //   Constructor
-    // Contexto (Activity), Lista(obtenemos por WS GET), listener (eventos de los botones
-
-    public MascotaAdapter ( Context context, ArrayList<Mascota> lista, OnAccionListener listener){
+    public MascotaAdapter (Context context, ArrayList<Mascota> lista, OnAccionListener listener){
         this.context = context;
         this.lista = lista;
         this.listener = listener;
     }
 
-
-    //Representara cada fila
-    static class ViewHolder extends  RecyclerView.ViewHolder{
+    static class ViewHolder extends RecyclerView.ViewHolder{
         TextView txtNombre, txtTipo, txtPeso;
         Button btnEditar, btnEliminar;
-
-        //Vinculacion
 
         ViewHolder(View itemview){
             super(itemview);
@@ -60,41 +43,34 @@ public class MascotaAdapter extends RecyclerView.Adapter<MascotaAdapter.ViewHold
         }
     }
 
-    //1 Inflar= crear layout para cada fila
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(context).inflate(R.layout.item_mascota, parent, false);
         return new ViewHolder(view);
     }
-    //Llenar los datos
-    @Override
-    public void onBindViewHolder(@NonNull MascotaAdapter.ViewHolder holder, int position) {
-        Mascota mascota = lista.get(position);
 
-        //TextView muestran los datos
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Mascota mascota = lista.get(position);
 
         holder.txtNombre.setText(mascota.getNombre());
         holder.txtTipo.setText(mascota.getTipo());
-        holder.txtPeso.setText(String.valueOf(mascota.getPesokg())+ "kg.");
+        holder.txtPeso.setText(mascota.getPesokg() + " kg");
 
-        //Botones
-        holder.btnEditar.setOnClickListener(v-> {
-            listener.onEditar(holder.getAdapterPosition(),mascota);
-        });
-        holder.btnEliminar.setOnClickListener(v -> {
-            listener.onEliminar(holder.getAdapterPosition(),mascota);
+        holder.btnEditar.setOnClickListener(v ->
+                listener.onEditar(holder.getAdapterPosition(), mascota)
+        );
 
-        });
-
+        holder.btnEliminar.setOnClickListener(v ->
+                listener.onEliminar(holder.getAdapterPosition(), mascota)
+        );
     }
-    //Calcular la cantidad de elementos de la lista
+
     @Override
     public int getItemCount() {
         return lista.size();
     }
-
-    //Eliminar un elemento de la lista <Mascota>
 
     public void eliminarItem(int position){
         lista.remove(position);
